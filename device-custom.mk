d6f820570ec2b1bcaa956c68436cbf1786e56e8b
razorloves <razorloves@gmail.com>
2019-05-26 04:14:33 -0500

    b1c1: Add missing packages and props for vendor image
    Change-Id: I88bdf8b2522b751c9490820b0ef55ee53dd35d1d

--- device-custom.mk
+++ device-custom.mk
@@ -3,7 +3,24 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Display
+PRODUCT_PACKAGES += \
+    libdisplayconfig
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    media.mediadrmservice.enable=true
 
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# Utilities
+PRODUCT_PACKAGES += \
+    libjson \
+    libtinyxml
+
+# WiFi
+PRODUCT_PACKAGES += \
+    libwifi-hal-qcom
