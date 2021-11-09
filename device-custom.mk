eb260b3f3eb311d58beabbd91ae53af5a53f549d
Nolen Johnson <johnsonnolen@gmail.com>
2021-11-09 15:24:12 -0500

    b1c1: Set camera extension package to Google package
    Change-Id: I519ebf8a1e2a9d06561471206c91c01b352ebd47

--- device-custom.mk
+++ device-custom.mk
@@ -26,6 +26,11 @@ PRODUCT_PACKAGES += \
     hardware.google.bluetooth.sar@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor
 
+# Camera Extensions
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
 # Context Hub Runtime Environment
 PRODUCT_PACKAGES += \
     chre
