fe3927b6510ec0adbae0606f76954a1f2495d6f7
Alexander Koskovich <akoskovich@pm.me>
2022-06-15 20:03:02 -0700

    b1c1: Move DTC definition to vendor/aosp
    Change-Id: I452ec4dabfd6abf052f020fe48527403df873f5f

--- BoardConfigCustom.mk
+++ BoardConfigCustom.mk
@@ -11,7 +11,6 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.lz4
 TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc
 TARGET_KERNEL_CONFIG := b1c1_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/bluecross
 
